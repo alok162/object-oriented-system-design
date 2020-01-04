4f61be8d87d261a56152b35a4e553d1a6c50e398
Alok Choudhary <alokchoudhary@Aloks-MacBook-Pro.local>
2020-01-04 19:32:38 +0530

    v1.0

--- hotel_management_system/main.py
+++ hotel_management_system/main.py
@@ -1,61 +1,3 @@
-from abc import ABC, abstractmethod
-
-
-class Person(ABC):
-    def __init__(self, name, email, phone):
-        self.__name = name
-        self.__email = email
-        self.__phone = phone
-
-
-class Account:
-    def __init__(self, id, password, status):
-        self.__id = id
-        self.__password = password
-        self.__status = status
-
-    def reset_password(self, password):
-        self.__password = password
-
-
-class Employee(Person):
-    def __init__(self, account: object, date_joined):
-        self.__account = account
-        self.__date_joined = date_joined
-
-
-class Receptionist(Employee):
-    def create_reservation(self):
-        pass
-
-    def search_customer(self):
-        pass
-
-    def register_customer(self):
-        pass
-
-
-class Manager(Employee):
-    def add_employee(self):
-        pass
-
-    def delete_employee(self):
-        pass
-
-
-class Waiter(Employee):
-    def take_order(self):
-        pass
-
-    def serve_order(self):
-        pass
-
-
-class Chef(Employee):
-    def prepare_order(self):
-        pass
-
-
 class Address:
     def __init__(self, street, city, zip_code, state, country):
         self.__street = street
@@ -65,86 +7,105 @@ class Address:
         self.__country = country
 
 
-class Branch:
-    def __init__(self, branch_name, address: object):
-        self.__branch_name = branch_name
+class Person:
+    def __init__(self, name, email, phone):
+        self.__name = name
+        self.__email = email
+        self.__phone = phone
+
+
+class Account:
+    def __init__(self, id, password, status, address):
+        self.__id = id
+        self.__password = password
+        self.__status = status
         self.__address = address
 
+    def reset_password(self, password):
+        self.__password = password
 
-class Restaurant:
+
+class Guest(Person):
+    def __init__(self, name, email, phone):
+        super().__init__(self, name, email, phone)
+
+    def get_booking(self):
+        pass
+
+
+class Receptionist(Person):
+    def __init__(self, name, email, phone):
+        super().__init__(self, name, email, phone)
+
+    def search_customer(self, customer):
+        pass
+
+    def register_customer(self, customer):
+        pass
+
+    def make_booking(self):
+        pass
+
+
+class HotelBranch:
+    def __init__(self, name, address: object):
+        self.__name = name
+        self.__address = address
+
+    def get_room(self):
+        pass
+
+
+class Hotel:
     def __init__(self, name, founded_year):
         self.__name = name
         self.__founded_year = founded_year
         self.__branches = []
 
-    def add_branch(self, branch: object):
-        self.__branches.append(branch)
+    def add_branch(self, hotel_branch):
+        self.__branches.append(hotel_branch)
 
-    def close_branch(self, branch):
-        self.__branches.remove(branch)
+    def remove_branch(self, hotel_branch):
+        self.__branches.remove(hotel_branch)
 
 
-class Table:
-    def __init__(self, table_id, max_capacity, location, seats: list):
-        self.__table_id = table_id
-        self.__max_capacity = max_capacity
-        self.__seats = seats
+class Room:
+    def __init__(self, id, floor_no, style, status, price, keys: list):
+        self.__id = id
+        self.__floor_no = floor_no
+        self.__style = style
+        self.__status = status
+        self.__price = price
+        self.__keys = keys
+        self.__check_in = None
+        self.__check_out = None
         self.__is_free = True
 
-    def book_table(self):
+    def update_checkout(self, checkout):
+        self.__check_out = checkout
+
+    def book_room(self):
         self.__is_free = False
 
 
-class Reservation:
-    def __init__(self, id, booking_time, no_of_people, status, notes, checkin_time,
-                 customer: object, table: object):
-        self.__id = id
-        self.__booking_time = booking_time
-        self.__no_of_people = no_of_people
+class Booking:
+    def __init__(self, booking_id, created_on, start_date, check_in, check_out,
+                 no_of_days, status, customer: object):
+        self.__booking_id = booking_id
+        self.__created_on = created_on
+        self.__start_date = start_date
+        self.__check_in = check_in
+        self.__check_out = check_out
+        self.__no_of_days = no_of_days
         self.__status = status
-        self.__notes = notes
-        self.__checkin_time = checkin_time
+        self.__rooms = []
         self.__customer = customer
-        self.__table = table
 
-    def update_no_of_people(self, count):
-        self.__no_of_people = count
+    def cancel_booking(self):
+        pass
 
+    def add_room(self, room: object):
+        self.__rooms.append(room)
 
-################################
-# restaurant menu
-class MenuItem:
-    def __init__(self, item_id, item_name, price, description):
-        self.__item_id = item_id
-        self.__item_name = item_name
-        self.__price = price
-        self.__description = description
-
-    def update_price(self, price):
-        self.__price = price
-
-
-class MenuSection:
-    def __init__(self, section_name, menu_items: list, description):
-        self.__section_name = section_name
-        self.__menu_items = menu_items
-        self.__description = description
-
-    def add_menu_item(self, item):
-        self.__menu_items.append(item)
-
-    def delete_menu_item(self, item):
-        self.__menu_items.remove(item)
-
-
-class Menu:
-    def __init__(self, title, description, menu_sections: list):
-        self.__title = title
-        self.__description = description
-        self.__menu_sections = menu_sections
-
-    def add_menu_section(self, menu_section):
-        self.__menu_sections.append(menu_section)
-
-    def remove_menu_section(self, menu_section):
-        self.__menu_sections.remove(menu_section)
+    def remove_room(self, room: object):
+        self.__rooms.remove(room)
